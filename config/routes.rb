2a19b0c75d257eb889c651c7651846e59e4a7ea8
joker99999 <jun.y8724@gmail.com>
2020-10-25 23:39:58 +0900

    テストユーザー機能１

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :targets
   resources :messages
   resources :users, only: [:index, :show]
+  # post '/homes/guest_sign_in', to: 'homes#new_guest'
   resources :shares do
     resources :sharecomments, only: [:create]
   end
