2b695ce950c2cae36c6a30ba3383acf3a886437c
joker99999 <jun.y8724@gmail.com>
2020-10-27 13:07:55 +0900

    テストユーザー

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,14 @@
 class HomesController < ApplicationController
+
   def index
   end
-
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.username = "guestuser"
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-  
-  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-  # devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
    }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  resources :users, only: [:index, :show]
   root to: "targets#index"
-  # get 'homes/index'
-  # get 'homes/show'
   resources :targets
   resources :messages
-  resources :users, only: [:index, :show]
-  # post '/homes/guest_sign_in', to: 'homes#new_guest'
   resources :shares do
     resources :sharecomments, only: [:create]
   end
