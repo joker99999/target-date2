6db8d09df05afcde96119f84a1f29b5c48517b4b
joker99999 <jun.y8724@gmail.com>
2020-11-01 18:36:51 +0900

    コメント　ajax

--- app/assets/javascripts/questioncomment.js
+++ app/assets/javascripts/questioncomment.js
@@ -1,30 +1,30 @@
-// $(function(){
-//   function buildHTML(comment){
-//     let html = `<p>
-//                   <strong>
-//                     <a href=/users/${comment.user_id}>${comment.user_name}</a>：
-//                   </strong>
-//                   ${comment.text}
-//                 </p>`
-//     return html;
-//   }
-//   $('#new_comment').on('submit', function(e){
-//     e.preventDefault();
-//     let formData = new FormData(this);
-//     let url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       let html = buildHTML(data);
-//       $('.comments').append(html);
-//       $('.textbox').val('');
-//       $('.submit').prop('disabled', false);
-//     })
-//   })
-// })
+$(function(){
+  function buildHTML(comment){
+    let html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
+  $('#new_questioncomment').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.questioncomments').append(html);
+      $('.question-textbox').val('');
+      $('.question-submit').prop('disabled', false);
+    })
+  })
+})
