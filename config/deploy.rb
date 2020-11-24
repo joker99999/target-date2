ed0ffb0c8b5dbb41f3afb9e4993ac596f8b740e0
joker99999 <jun.y8724@gmail.com>
2020-11-24 16:27:02 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'taeget-date2'
+set :application, 'target-date2'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:joker99999/target-date2.git'
